160feead7aab9b1b2a65cd8997148d40bde69161
Daniele Sabre <daniele.sabre@jakala.com>
2023-04-09 22:11:38 +0200

    develop

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-const msg = 'AppSidebar component';
 </script>
 
 <template>
-    <div class="p-2">{{ msg }}</div>
+    <div class="p-2">
+        <h2 class="font-bold text-lg">Turtle commands</h2>
+
+        <ul>
+            <li>
+                <span class="kbd">i</span>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style scoped>
+.kbd {
+    @apply inline-flex justify-center items-center border px-2;
+    border-color: hsl(var(--bc)/var(--tw-border-opacity));
+    --tw-border-opacity: .2;
+    --tw-bg-opacity: 1;
+    background-color: hsl(var(--b2,var(--b1))/var(--tw-bg-opacity));
+    border-radius: var(--rounded-btn,.5rem);
+    border-bottom-width: 2px;
+    min-height: 2.2em;
+    min-width: 2.2em;
+}
+</style>
